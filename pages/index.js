b7de71d461f926a12ae63578a7466765d447630c
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-04-14 14:08:07 +0530

    Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <div>Hello World</div>
+  return (
+    <div className='page-container'>
+      <div className={styles.main}>
+        <h1>NextJS News App</h1>
+        <h3>Your one stop shop for the latest news articles</h3>
+      </div>
+    </div>
+  )
 }
