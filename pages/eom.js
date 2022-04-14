fba81a041f54cf48d89140b9abbdebbd2071a31e
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-04-14 19:29:27 +0530

    Added Head in EOM page && UPDATED Readme

--- pages/eom.js
+++ pages/eom.js
@@ -1,19 +1,42 @@
+import Head from 'next/head'
 import styles from '../styles/EOM.module.css'
 
 export const eom = ({ employee }) => {
-  console.log(employee)
   return (
-    <div className='page-container'>
-      <div className={styles.main}>
-        <h1>Employee of the month</h1>
-        <div className={styles.employeeOfTheMonth}>
-          <h3>{employee.name}</h3>
-          <h6>{employee.position}</h6>
-          <img src={employee.image} alt='employee-img' />
-          <p>{employee.position}</p>
+    <>
+      <Head>
+        <title>Employee Of The Month</title>
+        <meta
+          name='description'
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+
+        <meta property='og:image' content={employee.image} />
+        <meta property='og:title' content='Employee Of The Month' />
+        <meta
+          property='og:description'
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+
+        <meta property='twitter:image' content={employee.image} />
+        <meta property='twitter:title' content='Employee Of The Month' />
+        <meta
+          property='twitter:description'
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+      </Head>
+      <div className='page-container'>
+        <div className={styles.main}>
+          <h1>Employee of the month</h1>
+          <div className={styles.employeeOfTheMonth}>
+            <h3>{employee.name}</h3>
+            <h6>{employee.position}</h6>
+            <img src={employee.image} alt='employee-img' />
+            <p>{employee.position}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
